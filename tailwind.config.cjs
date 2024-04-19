3043d7cf05255bd843165d87534f06b9e97c0b0a
cookiecollie <ediblechocolatething@gmail.com>
2024-04-19 15:52:31 +0700

    init: Project init

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./index.html", "./src/**/*.{html,ts,tsx}", "./src/*.{ts,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
