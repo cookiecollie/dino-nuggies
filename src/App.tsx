bfa2ae408451812022253034c2b6f02e88387b53
cookiecollie <ediblechocolatething@gmail.com>
2024-05-10 19:09:22 +0700

    update: Add animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,3 @@
-import { Tabs } from "./components/tabs"
-
 export const App = () => {
-    return (
-        <div className="w-[50%] h-full flex mx-auto items-center">
-            <div className="w-full h-96 border rounded-xl p-4 border-mine-shaft-200">
-                <p className="text-xl font-bold px-4">Tabs</p>
-                <hr className="py-2 mt-2" />
-                <Tabs>
-                    <Tabs.Labels>
-                        <Tabs.Label>Label 1</Tabs.Label>
-                        <Tabs.Label>Label 2</Tabs.Label>
-                        <Tabs.Label>Label 3 | Longer Label</Tabs.Label>
-                    </Tabs.Labels>
-
-                    <Tabs.Panels>
-                        <Tabs.Panel>Content 1</Tabs.Panel>
-                        <Tabs.Panel>Content 2</Tabs.Panel>
-                        <Tabs.Panel>Content 3</Tabs.Panel>
-                    </Tabs.Panels>
-                </Tabs>
-            </div>
-        </div>
-    )
+    return <div>this is from config branch</div>
 }
--- src/components/breadcrumb/Breadcrumb.tsx
+++ src/components/breadcrumb/Breadcrumb.tsx
@@ -1,23 +1,59 @@
-import { Children, PropsWithChildren, ReactNode } from "react"
+import { LayoutGroup, motion } from "framer-motion"
+import { ArrowLeft } from "lucide-react"
+import {
+    Children,
+    PropsWithChildren,
+    ReactNode,
+    useEffect,
+    useState,
+} from "react"
 
-interface BreadcrumbProps extends PropsWithChildren {}
+interface BreadcrumbProps extends PropsWithChildren {
+    onBackCallback?: () => void
+}
 
 export const Breadcrumb = (props: BreadcrumbProps) => {
-    const { children } = props
+    const { children, onBackCallback } = props
+
+    const childrenAsArray = Children.toArray(children)
+
+    const [showBack, setShowBack] = useState(false)
+
+    useEffect(() => {
+        if (childrenAsArray.length > 1) setShowBack(true)
+        else setShowBack(false)
+    }, [children])
 
     const renderChildren = Array<ReactNode>()
         .concat(
-            ...Children.toArray(children).map((c, i) => [
+            ...childrenAsArray.map((c, i) => [
                 c,
-                <li key={`separator-${i}`}>/</li>,
+                <motion.li layout key={`separator-${i}`}>
+                    /
+                </motion.li>,
             ])
         )
         .slice(0, -1)
 
     return (
-        <nav className="breadcrumb">
-            <ol>{renderChildren}</ol>
-        </nav>
+        <motion.nav className="breadcrumb">
+            <LayoutGroup>
+                {showBack && (
+                    <motion.a
+                        onClick={onBackCallback}
+                        className="arrow"
+                        layout
+                        initial={{ opacity: 0 }}
+                        animate={{ opacity: 1 }}
+                        exit={{ opacity: 0 }}
+                    >
+                        <ArrowLeft />
+                    </motion.a>
+                )}
+
+                <motion.ol layout>{renderChildren}</motion.ol>
+            </LayoutGroup>
+        </motion.nav>
     )
 }
 
@@ -27,11 +63,11 @@ interface CrumbProps extends PropsWithChildren {
 const Crumb = (props: CrumbProps) => {
     const { children, href } = props
     return (
-        <li className="crumb">
+        <motion.li className="crumb" layout>
             <a href={href} className="link" data-is-link={href != null}>
                 {children}
             </a>
-        </li>
+        </motion.li>
     )
 }
 Breadcrumb.Crumb = Crumb
