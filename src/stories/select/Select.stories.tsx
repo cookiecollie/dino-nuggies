2a1da9ab8b0ca5fc81ce44368f6ba4cfad77be3f
cookiecollie <ediblechocolatething@gmail.com>
2024-05-21 20:49:05 +0700

    update: Include Virtualized Select to story

--- src/stories/select/Select.stories.tsx
+++ src/stories/select/Select.stories.tsx
@@ -8,24 +8,48 @@ import { Option } from "../../components/select/Select"
 import countries from "../../mock/countries.json"
 
 export default {
-    component: VirtualizedSelect,
-} as Meta<typeof VirtualizedSelect>
+    component: Select,
+    subcomponents: { VirtualizedSelect },
+} as Meta<typeof Select>
 
-type Story = StoryObj<typeof VirtualizedSelect>
+type SelectStory = StoryObj<typeof Select>
+type VirtualizedSelectStory = StoryObj<typeof VirtualizedSelect>
 
-export const Virtualized: Story = {
+export const Virtualized: VirtualizedSelectStory = {
     render: ({ ...args }) => (
-        <div className="min-w-[25%] w-fit">
-            <VirtualizedSelect {...args}>
-                {countries.map((c) => (
-                    <VirtualizedOption key={c.code}>{c.name}</VirtualizedOption>
-                ))}
-            </VirtualizedSelect>
+        <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2">
+                <p>Countries list</p>
+
+                <div className="min-w-[25%] w-fit">
+                    <VirtualizedSelect {...args}>
+                        {countries.map((c) => (
+                            <VirtualizedOption key={c.code}>
+                                {c.name}
+                            </VirtualizedOption>
+                        ))}
+                    </VirtualizedSelect>
+                </div>
+            </div>
+
+            <div className="flex flex-col gap-2">
+                <p>10000 items</p>
+
+                <div className="min-w-[25%] w-fit">
+                    <VirtualizedSelect {...args}>
+                        {Array.from({ length: 10000 }, (item, idx) => (
+                            <VirtualizedOption key={idx}>
+                                {`Item ${idx}`}
+                            </VirtualizedOption>
+                        ))}
+                    </VirtualizedSelect>
+                </div>
+            </div>
         </div>
     ),
 }
 
-export const Default: Story = {
+export const Default: SelectStory = {
     render: ({ ...args }) => (
         <div className="min-w-[25%] w-fit">
             <Select {...args}>
