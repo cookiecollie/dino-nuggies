e90cbd55a8e6e06a90f49e3ad45c5da9ff45f8bd
cookiecollie <ediblechocolatething@gmail.com>
2024-06-01 00:54:30 +0700

    update: Add utilities

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,19 @@
+import { ItemList, useListItem } from "./ItemList"
+export { ItemList, useListItem }
+
+export const mergeRefs = <T>(
+    refs: Array<React.MutableRefObject<T> | React.LegacyRef<T> | null>
+): React.RefCallback<T> => {
+    return (value) => {
+        refs.forEach((ref) => {
+            if (typeof ref === "function") {
+                ref(value)
+            } else if (ref != null) {
+                ;(ref as React.MutableRefObject<T | null>).current = value
+            }
+        })
+    }
+}
 export { type PolymorphicProps } from "./PolymorphicProps";
 
 import { ItemList, useListItem } from "./ItemList"
